fc23912c3f1a631f9620c18af93aa66471783af0
sungkim3 <sng.kim103@gmail.com>
2016-04-15 13:35:28 -0700

    updates app.js to include a sumArray function

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ function sumAndMultiply(number1, number2, number3) {
   console.log('"The numbers ' + number1 + ' and ' + number2 + ' and ' + number3 + ' have a product of ' + product + '."');
   return [add, product];
 }
+
+function sumArray(numberArray) {
+  var total = 0;
+  for (i = 0; i < numberArray.length; i++) {
+    total += numberArray[i];
+  }
+  console.table(numberArray);
+  console.log(numberArray.join(', ') + ' was passed in as an array of numbers, and ' + total + ' is their sum.');
+  return total;
+}
