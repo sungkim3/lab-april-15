2d141639145eaf31570c421b08a95b715eb6ad01
sungkim3 <sng.kim103@gmail.com>
2016-04-15 12:59:36 -0700

    updates app.js to include the console.log message as well as includes the multiply function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function sum(number1, number2) {
-  return number1 + number2;
+  return '"The sum of ' + number1 + ' and ' + number2 + ' is ' + (number1 + number2) + '."';
+}
+
+function multiply(number1, number2) {
+  return '"The product of ' + number1 + ' and ' + number2 + ' is ' + (number1 * number2) + '."';
 }
